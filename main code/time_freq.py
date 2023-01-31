6858106e00aacf779e20fa2d0ef2615834301aef
franciumxzf <francium0415@gmail.com>
2023-01-31 22:05:15 +0800

    Update time_freq.py

--- main code/time_freq.py
+++ main code/time_freq.py
@@ -1,14 +1,15 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from scipy.fft import fft, ifft
 import sys
 
-UPPER_LIMIT = 5e-5 # put as an option
+UPPER_LIMIT = 5e-5 # whether to put as an option
 TIMESTAMP_RESOLUTION = 256
+S_th = 6 #significance limit
 Ta = 2**29 #acquisition time interval/ num of periods/ epochs
 Ts = 6 * Ta #separation time interval
-S_th = 6 #significance limit
 delta_tmax = 1 # the maximum acceptable delta_t to start tracking/ timing resolution
 N = 2**20 #bin number, note that this N remains unchanged during the whole algorithm/ buffer order
 
@@ -23,21 +24,33 @@ def read_a1(filename: str, legacy: bool = False):
 def cross_corr(arr1, arr2):
     return ifft(np.conjugate(fft(arr1)) * fft(arr2))
 
+# Update:
+# 1. use only the real part from the cross correlation to calculate find_max and statistical_significance
+# 2. shall we calculate sigma using the same method as pfind.c or just this statistical significance?
+
 def find_max(arr):
+    arr_real = arr.real
     time_diff = 0
-    max_value = arr[time_diff]
-    for i in range(len(arr)):
-        if arr[i] > max_value:
+    max_value = arr_real[time_diff]
+    arr_sum = 0
+    power_sum = 0
+    for i in range(len(arr_real)):
+        arr_sum += arr_real[i]
+        power_sum += arr_real[i] ** 2
+        if arr_real[i] > max_value:
             time_diff = i
-            max_value = arr[i]
-    if time_diff > (np.size(arr) / 2):
-        time_diff -= np.size(arr)
-    return time_diff, max_value
+            max_value = arr_real[i]
+    if time_diff > (len(arr_real) / 2):
+        time_diff -= len(arr_real)
+    arr_mean = arr_sum / len(arr_real)
+    sigma = math.sqrt(power_sum / len(arr_real) - arr_mean ** 2)
+    return time_diff, max_value, sigma
 
 def statistical_significance(arr):
-    ck = np.max(arr)
-    ck_average = arr.sum() / np.size(arr)
-    S = (ck - ck_average) / np.std(arr)
+    arr_real = arr.real
+    ck = np.max(arr_real)
+    ck_average = arr_real.sum() / np.size(arr_real)
+    S = (ck - ck_average) / np.std(arr_real)
     return S
 
 def process_timestamp1(arr, start_time, delta_t):
@@ -69,7 +82,7 @@ def time_freq(arr_a, arr_b):
     
     while statistical_significance(arr_c1) <= S_th:
         if delta_t > 5e6: # if delta_t goes too large and still cannot find the peak, means need to change the hyperparameters
-            print("Cannot successfully find the peak!")
+            print("Cannot successfully find the peak!", file = sys.stderr)
             raise ValueError
         #halve the size of the array {ck} by adding entries pairwise
         arr_c1 = np.sum(arr_c1.reshape(-1, 2), axis = 1)
@@ -102,7 +115,7 @@ def time_freq(arr_a, arr_b):
         
         print(delta_T1, 0, file = sys.stderr)
         return delta_T1, 0
-
+ 
     arr_a -= T_start
     arr_b -= T_start
     T_start = 0
@@ -130,7 +143,7 @@ def time_freq(arr_a, arr_b):
         delta_u_correct = (delta_T1_correct - delta_T2_correct) / Ts
         u *= (1 / (1 - delta_u_correct))
 
-        if statistical_significance(new_arr_c1) < 6: # if the peak is too low, high likely the result is wrong
+        if statistical_significance(new_arr_c1) < 6: # if the peak is too low, high likely the result is wrong - there might be a problem that the delta_t doesn't go to 1
             break
 
     delta_u = 1 / u - 1
@@ -214,8 +227,11 @@ def pfind(alice, bob): # perform plus and minus step size concurrently
         backup_bob /= (1 + UPPER_LIMIT)
         freq_result = UPPER_LIMIT
         return pfind(alice, backup_bob)
-    print(time_result, freq_result, file = sys.stderr) # here actually is just stdout, but we need to insert the standard deviation here
+    print(f"{int(time_result):d}\t{int(freq_result * 1e12):d}\n", file = sys.stdout)
     return time_result, freq_result
+
+# time result: units of 1ns
+# frequency result: units of 1e-12
 
 def result(alice_timestamp, bob_timestamp): #remember to change legacy setting, default is False
     alice = read_a1(alice_timestamp)
@@ -224,5 +240,31 @@ def result(alice_timestamp, bob_timestamp): #remember to change legacy setting, 
     alice = read_a1(alice_timestamp)
     bob = read_a1(bob_timestamp)
     bob_time, bob_result = pfind(alice, bob) # alice as reference
-    print((alice_result + 1) * (bob_result + 1)) # prove that frequency results converge
-    print(alice_time * (1 + alice_result) + bob_time) # prove that time offset results converge
+    print((alice_result + 1) * (bob_result + 1), file = sys.stderr) # prove that frequency results converge
+    print(alice_time * (1 + alice_result) + bob_time, file = sys.stderr) # prove that time offset results converge
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "")
+    parser.add_argument("-d", help="RECEIVEFILES")
+    parser.add_argument("-D", help="T1FILES")
+    # parser.add_argument("-e", type = float, help="first overlapping epoch between the two remotes") # need to add this back when receive file from chopper
+    parser.add_argument("-n", type = int, help = "number of epochs, Ta")
+    parser.add_argument("-V", help = "verbosity")
+    parser.add_argument("-q", type = int, help = "FFT buffer order, N")
+    parser.add_argument("-r", help="desired timing resolution")
+
+
+    if len(sys.argv) > 1:
+        args = parser.parse_args()
+
+        alice = read_a1(args.d, legacy = True)
+        bob = read_a1(args.D, legacy = True)
+
+        Ta = args.n
+        # T_start = args.e # maybe change in each iteration
+        delta_tmax = args.r
+        N = args.q
+        Ts = 6 * Ta
+
+        pfind(alice, bob)
