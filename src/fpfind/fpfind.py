fd474c9259262021a33ae45e20114eb566ac7f3d
pyuxiang <pehyuxiang@gmail.com>
2024-01-22 12:12:59 +0800

    Push minimal working code post-refactoring

--- src/fpfind/fpfind.py
+++ src/fpfind/fpfind.py
@@ -18,7 +18,7 @@ import configargparse
 import numpy as np
 
 from fpfind.lib.parse_timestamps import read_a1
-from fpfind.lib.utils import generate_fft, get_timing_delay_fft, slice_timestamps, get_xcorr, get_statistics
+from fpfind.lib.utils import round, generate_fft, get_timing_delay_fft, slice_timestamps, get_xcorr, get_statistics
 
 from fpfind.lib.utils import (
     ArgparseCustomFormatter, LoggingCustomFormatter,
@@ -91,54 +91,67 @@ def time_freq(
     # TODO: Be careful of slice timestamps
     # BOOKKEEPING
     target_resolution = resolution
-    start_time = max(ats[0], bts[0])
+    start_time = 0
     duration = num_wraps * resolution * num_bins
 
     logger.debug(
-        "Running with 2^%d bins of width %.0f ns, with start bias %.1e ns",
-        np.int32(np.log2(num_bins)), resolution, start_time/1e9,
-    )
-    logger.debug(
-        "Biased: ats = %s...%s, bts = %s...%s",
-        ats[[0,1]] - start_time,
-        ats[[-1]] - start_time,
-        bts[[0,1]] - start_time,
-        bts[[-1]] - start_time,
+        "Running with 2^%d bins of width %.0f ns",
+        np.int32(np.log2(num_bins)), resolution,
     )
 
     # Quit if df is near zero, i.e. no need to correct
     dt = 0
     df1 = 0
     f = 1
-    is_peak_found = False
+    curr_iteration = 1
     while True:
+        logger.debug("Current fpfind iteration = %s", curr_iteration)
+        logger.debug(
+            "  Alice timing range: [%s, %s]s",
+            round((ats[0] - start_time) * 1e-9, 2),
+            round((ats[-1] - start_time) * 1e-9, 2),
+        )
+        logger.debug(
+            "  Bob timing range: [%s, %s]s",
+            round((bts[0] - start_time) * 1e-9, 2),
+            round((bts[-1] - start_time) * 1e-9, 2),
+        )
+        if curr_iteration == 1:
+            logger.debug("  Target resolution = %.0f ns", resolution)
+        else:
+            logger.debug("  Current resolution = %.0f ns", resolution)
 
         # Earlier cross-correlation
-        logger.debug("Performing earlier xcorr")
+        logger.debug(
+            "  Performing earlier xcorr (range: [%s, %s]s)",
+            round(0, 2),
+            round((duration)*1e-9, 2),
+        )
+
         afft = generate_fft(
             slice_timestamps(ats, start_time, duration),
             num_bins, resolution)
+
         bfft = generate_fft(
             slice_timestamps(bts, start_time, duration),
             num_bins, resolution)
         ys = get_xcorr(afft, bfft)
 
         # Confirm resolution on first run
-        while not is_peak_found:
-            logger.debug("Performing first peak observation")
+        while curr_iteration == 1:
+            logger.debug("  Performing first peak observation")
             stats = get_statistics(ys, resolution)
-            logger.debug("S = %6.3f, dT = %s ns", stats.significance, dt)
+            logger.debug("    S = %6.3f, dT = %s ns", stats.significance, dt)
             if stats.significance == 0:
                 raise ValueError("Flatlined, need to increase number of bins")
             if stats.significance >= threshold:
-                is_peak_found = True
-                logger.debug("Peak found, with resolution %.0f ns", resolution)
+                logger.debug("    Peak found, with resolution %.0f ns", resolution)
                 break
             
             # Increase the bin width
             ys = np.sum(ys.reshape(-1, 2), axis = 1)
             resolution *= 2
-            logger.debug("Doubling resolution to %.0f ns", resolution)
+            logger.debug("    Doubling resolution to %.0f ns", resolution)
             if resolution > 5e6:
                 raise ValueError("Number of bins too little.")
 
@@ -149,7 +162,12 @@ def time_freq(
             break
 
         # Later cross-correlation
-        logger.debug("Performing later xcorr")
+        logger.debug(
+            "  Performing later xcorr (range: [%s, %s]s)",
+            round((separation_duration)*1e-9, 2),
+            round((separation_duration+duration)*1e-9),
+        )
+
         _afft = generate_fft(
             slice_timestamps(ats, start_time + separation_duration, duration),
             num_bins, resolution)
@@ -162,18 +180,15 @@ def time_freq(
         xs = np.arange(num_bins) * resolution
         dt1 = get_timing_delay_fft(ys, xs)[0]
         _dt1 = get_timing_delay_fft(_ys, xs)[0]
-        logger.debug(
-            "dt1 = %.0f ns, and _dt1 = %.0f ns",
-            dt1, _dt1,
-        )
+        logger.debug("  Calculate timing delays")
+        logger.debug("    early dt = %.0f ns", dt1)
+        logger.debug("    late  dt = %.0f ns", _dt1)
 
         # dt = dt / (1 + df1) + dt1  # update with old frequency. TODO: Verify.
         df1 = (_dt1 - dt1) / separation_duration
+        logger.debug("    current df = %.3f ppm", df1 * 1e6)
+        # TODO: Short-circuit later xcorr if frequency difference is already zero, but need to be careful when array is flatlined
         dt = (dt + dt1) / (1 + df1)
-        logger.debug(
-            "Current estimation: dt = %.0f ns, df = %.3f ppm, separation_duration = %s",
-            dt1, df1 * 1e6, separation_duration,
-        )
         f *= 1 / (1 + df1)
 
         # Stop if resolution met, otherwise refine resolution
@@ -182,11 +197,12 @@ def time_freq(
 
         resolution = resolution / (separation_duration / duration / np.sqrt(2))
         bts = (bts - dt1) / (1 + df1)  # update from current run
+        curr_iteration += 1
 
     df = 1 / f - 1
-    logger.debug("Returning: dt = %.0f ns, df = %.3f ppm",
-        dt, df * 1e6,
-    )
+    logger.debug("Final calculated result:")
+    logger.debug("  dt = %.0f ns", dt)
+    logger.debug("  df = %.3f ppm", df * 1e6)
     return dt, df
 
 @profile
@@ -224,6 +240,7 @@ def fpfind(alice, bob,
         # Apply frequency precompensation df0
         # TODO: CONTINUE
         df = df0
+        # TODO: The frequency compensation needs to be normalized to start time before applying! Better I set the reference all the way in the beginning, then no need to keep slicing up the timestamps.
         dt, df1 = time_freq(alice, bob / (1 + df), num_wraps, resolution, num_bins, threshold, separation_duration)
 
         # Apply additional frequency compensation
@@ -306,7 +323,7 @@ def main():
         "-r", "--resolution", type=int, default=16,
         help="Specify desired timing resolution, in units of ns.")
     parser.add_argument(
-        "-s", "--separation", type=int, default=6,
+        "-s", "--separation", type=float, default=6,
         help="Specify width of separation, in units of epochs.")
     parser.add_argument(
         "-S", "--threshold", type=float, default=6,
@@ -326,8 +343,12 @@ def main():
     # skip_epoch = args.skip_epochs
     # num_of_epochs = args.num_epochs
     skip_epoch = 0
-    num_of_epochs = args.resolution * (1 << args.buffer_order) / (1 << 29)
+    Ta = args.resolution * (1 << args.buffer_order)
+    num_of_epochs = Ta / (1 << 29)
     separation_width = args.separation
+
+    logger.debug("xcorr duration = %.1f s", Ta * 1e-9)
+    logger.debug("Total required duration = %.1f s", (separation_width + 1) * Ta * 1e-9)
 
     # fmt: off
 
@@ -344,6 +365,8 @@ def main():
         logger.debug("Reading from timestamp files.")
         ta = read_a1(args.target, legacy=True)[0]
         tb = read_a1(args.reference, legacy=True)[0]
+        # ta = read_a1(args.target, legacy=False)[0]
+        # tb = read_a1(args.reference, legacy=False)[0]
         # TODO: Parse ta[0] as epoch value then split from there
         # offset_start = skip_epoch*EPOCH_LENGTH
         # offset_end = offset_start + num_of_epochs*EPOCH_LENGTH
@@ -368,10 +391,25 @@ def main():
         logger.error("Timestamp files/epochs must be supplied with -tT/-dD")
         sys.exit(1)
 
-    Ta = num_of_epochs * EPOCH_LENGTH
+    #Ta = num_of_epochs * EPOCH_LENGTH
     DELTA_U_MAX = 0
     DELTA_U_STEP = 1e-6
-    bob = bob * (1 + 50e-9)
+    # bob = bob * (1 + 50e-9)
+    # bob = bob[1:]
+    # bob = bob * (1 - 100e-6)
+
+    # Normalize to common time reference near start, so that
+    # frequency compensation will not shift the timing difference too far
+    start_time = max(alice[0], bob[0])
+    alice = slice_timestamps(alice, start_time)
+    bob = slice_timestamps(bob, start_time)
+
+    # Apply a frequency adjustment
+    # f = -30e-6
+    # bob = bob * (1 + f)
+    globals().update(locals())
+
+    
 
     logger.debug("Triggered fpfind main routine.")
     td, fd = fpfind(
